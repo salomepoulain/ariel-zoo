97b16d4f0202a2aacdf3fa9181f27859d6de4dd0
Jacopo-DM <jacopo.michele.dimatteo@gmail.com>
2025-10-13 00:29:54 +0200

    BugFix: when using only ReLU, in absence of a normalizing 'tanh' for example, the outputs can explode to very high values, forcing the final sigmoid layer to always output 1 or 0.

--- src/ariel/ec/genotypes/nde/nde.py
+++ src/ariel/ec/genotypes/nde/nde.py
@@ -96,6 +96,7 @@ class NeuralDevelopmentalEncoding(nn.Module):
 
         # Activations
         self.relu = nn.ReLU()
+        self.tanh = nn.Tanh()
         self.sigmoid = nn.Sigmoid()
 
         # Disable gradients for all parameters
@@ -115,7 +116,7 @@ class NeuralDevelopmentalEncoding(nn.Module):
                 x = self.relu(x)
 
                 x = self.fc2(x)
-                x = self.relu(x)
+                x = self.tanh(x)
 
                 x = self.fc3(x)
                 x = self.relu(x)
